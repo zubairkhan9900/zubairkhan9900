7fc2b8eb93dbea0421a31a1a7fa890f201a701eb
zubairkhan9900 <zubairkhanrdxx@gmail.com>
2024-02-13 13:57:51 +0530

    done

--- Ad javascript/test.js
+++ Ad javascript/test.js
@@ -7,15 +7,16 @@
 //   console.log("-----------  rerturnMsg----------->", rerturnMsg);
 //   print();
   
-  const print2 = (no1, no2, no3) => {
-    console.log("-----------  no1----------->", no1);
-    console.log("-----------  no2----------->", no2);
-    console.log("-----------  no3----------->", no3);
-    console.log("----print2-----");
-  };
   
-  print2(10, 20);
+  // print2(10, 20);
   
-  let arr = [1, 2, 3, 4, 5, 6];
-  let newArr = arr.toSpliced(1, 2);s
-  console.log("-----------  newArr----------->", newArr);
+  // let arr = [1, 2, 3, 4, 5, 6];
+  // let newArr = arr.toSpliced(1, 2);s
+  // console.log("-----------  newArr----------->", newArr);
+
+  const contactdetails = (username,MobileNo,Emailid) => {
+    console.log("username -",username);
+    console.log("MobileNo -",MobileNo);
+    console.log("Emailid -",Emailid);
+  }
+  contactdetails("zubair","9328546894","zubairkhanrdxx@gmail.com")
